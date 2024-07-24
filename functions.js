3c9ac4d8b5eb9459f32f196ba6ebd9bd6bd65235
Ashutosh Yadav <ashu0705000@gmail.com>
2024-07-24 20:00:43 +0000

    adding higher order functions file

--- functions.js
+++ functions.js
@@ -1,7 +1,13 @@
-const func = function(a,b){
-        return a+b
-}
-console.log(func(2,3))
+// const func = function(a,b){
+//         return a+b
+// }
+// console.log(func(2,3))
 
-console.log(func())
+// console.log(func())
 
+// func1 = (a,b) =>{
+//         console.log(this)
+//         return a+b;
+// }
+func1 = (a,b) => (a+b) 
+console.log(func1(2,3))
---
+++ higher_order_func.js
@@ -0,0 +1,23 @@
+// let arr = [0,2,3,4,5]
+
+// for(let i of arr)
+//         console.log(i)
+
+// for(let i in arr)
+//     console.log(i)
+
+// let obj = {
+//     name:"ashutosh",
+//     age : "24"
+// }
+
+// for(let i in obj) //cannot use of
+//     console.log(i)
+
+let map = new Map()
+
+map.set("name","ashutosh")
+map.set("age","24")
+
+for(let [key,value] of map) //cannot use in 
+    console.log(`${key} -> ${value}`)
